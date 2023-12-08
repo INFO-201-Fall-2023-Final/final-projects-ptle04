d421cab1328c310537b4c0b2b5aac88cd7c39f3e
ptle04 <146246542+ptle04@users.noreply.github.com>
2023-12-08 12:11:07 -0800

    Update Bubble chart code.R

--- Bubble chart code.R
+++ Bubble chart code.R
@@ -1,28 +1,20 @@
-# Install and load necessary packages if not already installed
-if (!requireNamespace("ggplot2", quietly = TRUE)) {
-  install.packages("ggplot2")
-}
-if (!requireNamespace("dplyr", quietly = TRUE)) {
-  install.packages("dplyr")
-}
 
 # Load required libraries
 library(ggplot2)
 library(dplyr)
+source("data_cleanup.R")
 
-# Sample data (replace this with your actual data)
-# Assuming your data frame is named 'suicide_data' with columns 'Year' and 'Number_of_Suicides'
-# Make sure your data includes the years 2007 to 2017
-suicide_data <- data.frame(
-  Year = c(2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017),
-  Number_of_Suicides = c(100, 120, 90, 110, 130, 150, 140, 160, 180, 200, 220)
-)
-
+bub_df <-  group_by(suicide_df, YEAR, ESTIMATE)
+bub_df <- filter(suicide_df, STUB_LABEL == "All persons")
+bub_df <- filter(bub_df, YEAR >= 2007)
+bub_df <- filter(bub_df, YEAR <= 2017)
+bub_df <- filter(bub_df, UNIT_NUM == 1)
 # Create a bubble chart using ggplot2
-ggplot(suicide_data, aes(x = Year, y = Number_of_Suicides, size = Number_of_Suicides)) +
-  geom_point(alpha = 0.7, color = "blue") +
+bubble <- ggplot(data = bub_df, aes(x = YEAR, y = ESTIMATE, size = ESTIMATE, color = ESTIMATE))+ 
+  geom_point() +
   labs(title = "Total Number of Suicides Each Year (2007-2017)",
        x = "Year",
-       y = "Number of Suicides") +
+       y = "Number of Suicides (per 100,000 resident population)") +
   scale_size_continuous(range = c(3, 10)) +
   theme_minimal()
+plot(bubble)
