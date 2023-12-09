0e8fb81535ca631e09f03b034bd945ae04d559cf
ptle04 <146246542+ptle04@users.noreply.github.com>
2023-12-08 21:04:35 -0800

    Update Area.R

--- Area.R
+++ Area.R
@@ -1,7 +1,25 @@
-# Graph 2: Area Chart
-suicide_rate_df <- df[, c("YEAR", "ESTIMATE")]
-suicide_rate_df <- aggregate(Average_Estimate ~ YEAR, data = suicide_rate_df, FUN = mean)
+#import libraries/sources
 
-ggplot(suicide_rate_df, aes(x = YEAR, y = Average_Estimate)) +
+library(ggplot2)
+library(dplyr)
+library(plotly)
+source("data_cleanup.R")
+
+# Graph 2: Area Chart
+
+#suicide_rate_df <- df[, c("YEAR", "ESTIMATE")]
+#suicide_rate_df <- aggregate(Average_Estimate ~ YEAR, data = suicide_rate_df, FUN = mean)
+
+#suicide_rate_df <- group_by(df, YEAR, ESTIMATE)
+suicide_rate_df <- group_by(df, YEAR)
+suicide_rate_df <- summarize(suicide_rate_df, Average_Estimate = mean(ESTIMATE, na.rm = TRUE))
+
+area <- ggplot(suicide_rate_df, aes(x = YEAR, y = Average_Estimate)) +
   geom_area(fill = "blue", alpha = 0.5) +
-  labs(title = "Change in Suicide Death Rates Over Time", x = "Year", y = "Average Suicide Death Rate"
+ labs( x = "Year", y = "Average Suicide Death Rate")
+
+area_plotly <- ggplotly(area)
+
+plot(area)
+
+
