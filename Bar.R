4d31590d45d56265deb4a5d93b93af4729103871
ptle04 <146246542+ptle04@users.noreply.github.com>
2023-12-08 21:05:27 -0800

    Update Bar.R

--- Bar.R
+++ Bar.R
@@ -1,9 +1,45 @@
-# Graph 1: Multi Bar Chart
-top_causes_df <- df[df$Cause.Name != "Suicide", ]
-top_causes_df <- aggregate(Total_Deaths ~ Cause.Name, data = top_causes_df, FUN = sum)
-top_causes_df <- top_causes_df[order(-top_causes_df$Total_Deaths), ]
-top_causes_df <- head(top_causes_df, 3)
+import libraries/sources
+library(ggplot2)
+library(dplyr)
+library(plotly)
+library(ggpubr)
+source("data_cleanup.R")
 
-ggplot(top_causes_df, aes(x = Cause.Name, y = Total_Deaths)) +
-  geom_bar(stat = "identity") +
-  labs(title = "Top 3 Causes of Death (Excluding Suicide)", x = "Cause of Death", y = "Number of Deaths")
+#find differences
+point <- group_by(df, Cause.Name) 
+point <- point[!duplicated(df$Deaths), ]
+
+causes <- unique(point$Cause.Name)
+
+death_counts <- numeric(length(causes))
+
+for (i in seq_along(causes)) {
+  death_counts[i] <- sum(point$Deaths[point$Cause.Name == causes[i]])
+}
+
+# Combine the causes and their counts into a data frame
+result <- data.frame(cause = causes, deaths = death_counts)
+
+base_value <- result$deaths[4]  # if the base value is the first value in the column
+difference <- result$deaths - base_value
+
+# Add the difference to the dataframe as a new column
+result$difference <- difference
+result <- filter(result, cause != "Suicide")
+
+# Lollipop chart
+lolli <- ggplot(result, aes(x= cause, y= deaths)) +
+  geom_segment( aes(x=cause, xend=cause, y=base_value, yend=difference), color="grey") +
+  geom_point( color="orange", size=4) +
+  geom_vline(aes(xintercept = base_value), color = "black", linetype = "solid")+
+  theme_light() +
+  theme(
+    panel.grid.major.x = element_blank(),
+    panel.border = element_blank(),
+    axis.ticks.x = element_blank()
+  ) +
+  xlab("Difference in death from Suicide") +
+  ylab("Cause of Death")+
+  coord_flip()
+
+plot(lolli)
